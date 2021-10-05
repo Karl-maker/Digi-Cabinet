1e9ab0cd7fe906a7b4b4c96842e2d9b264db6213
karl-maker <karljohanbailey98@gmail.com>
2021-10-05 18:18:52 -0400

    Restructure

--- backend/src/controller/controllers/test.mjs
+++ backend/src/controller/controllers/test.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/test", (req, res, next)=>{
+router.get("/", (req, res, next)=>{
     res.status(200).json({ message: "Welcome to Digi-Connect API Test" , 
     timestamp: `${new Date().toString()}` });
 });
--- backend/src/controller/index.mjs
+++ backend/src/controller/index.mjs
@@ -8,6 +8,6 @@ import express from "express";
 import test from "./controllers/test.mjs";
 const router = express.Router();
 
-router.use(test);
+router.use("/test", test);
 
 export default router;
