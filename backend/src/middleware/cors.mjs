269b5fbb2466ad84513e0088e6625f3be57ed326
karl-maker <karljohanbailey98@gmail.com>
2021-10-05 22:10:28 -0400

    Middlewares
    Added Modules for security and functionality

---
+++ backend/src/middleware/cors.mjs
@@ -0,0 +1,11 @@
+export const corsOrigins = {
+    origin: "*",
+    //origin: function (origin, callback) {
+    // db.loadOrigins is an example call to load
+    // a list of origins from a backing database
+    //db.loadOrigins(function (error, origins) {
+    //callback(error, origins)
+    //})
+    //}
+  };
+  
--- backend/src/server/server.mjs
+++ backend/src/server/server.mjs
@@ -2,18 +2,48 @@ import logger from "../log/server-logger.mjs";
 import httpLogger from "../log/http-logger.mjs";
 import routes from "../controller/index.mjs";
 import config from "../config/config.mjs";
+import { corsOrigins } from "../middleware/cors.mjs";
 ///-----------------------------------------------
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 //utilites
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const limiter = rateLimit({
+    windowMs: config.optimization.RATE_LIMIT_WINDOWMS * 60 * 1000,
+    max: config.optimization.RATE_LIMIT_MAX,
+  });
 const initialize = (app, { express }) => {
 
     app.use(httpLogger); 
+    app.use(helmet());
+    app.use(limiter);
+    app.use(urlencodedParser);
+    app.use(jsonParser);
+    app.use(cookieParser());
+    app.use(
+        cors({
+          origin: corsOrigins.origin,
+          optionSuccessStatus: 200,
+        })
+      );
+
+    /*
+    ----------------------------------------------------------------
+    *****************Static and API routes below********************
+    ----------------------------------------------------------------
+    */
+
+
     app.use("/api", routes);
-    //Static Route
     app.use(
         "/container",
         express.static(
