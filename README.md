d075d38393703ae2cce9329b69a9ecc8748d3340
karl-maker <karljohanbailey98@gmail.com>
2021-10-09 19:36:27 -0400

    Configuration Upgrade
    Less code is needed now because of the cross-env and dotenv-flow npm packages

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### On use of the node.js application the .env files must be filled to connect to MongoDB database and any other dependencies needed such as Redis and other services (example: Nodemailer)
 
-#### Create a file named .env in backend folder and copy and paste then fill in all variables in .example.env (varibles with * by it are optional)
+##### Create a file named .env OR other .env types in backend folder and copy and paste then fill in all variables in .example.env (varibles with * by it are optional)
 
 #### in terminal follow instructions to run backend:
 
--- backend/src/config/config.mjs
+++ backend/src/config/config.mjs
@@ -3,27 +3,51 @@ Author: Karl-Johan Bailey 05/10/2021
 
 This file must controll configuration throughout the app with environment awareness.
 */
+import dotenv from "dotenv-flow";
 
-
-import test from "./env/test.mjs";
-import development from "./env/development.mjs";
-import production from "./env/production.mjs";
-
-const env = process.env.NODE_ENV || "development";
+dotenv.config();
+const ENV = process.env;
 
 //Default Variables
 
-const defaults = {
+const variables = {
   environment: {
-    NODE_ENV: env || "development",
-    RESOURCE_PATH: process.env.RESOURCE_PATH || "../container",
+    RESOURCE_PATH: ENV.RESOURCE_PATH || "../container",
   },
+
+  //Server API:
+  server: {
+    PROTOCOL: ENV.PROTOCOL || "http",
+    HOST: ENV.API_HOST || "localhost",
+    PORT: ENV.API_PORT || 8000,
+  },
+
+  debug: {
+    LOG_FILE: ENV.LOG_FILE || "./logs/server.log", //Relative to Log folder in source
+    LOG_MAXSIZE: ENV.LOG_MAXSIZE || 5242880, //5MB
+    LOG_MAXFILES: ENV.LOG_MAXFILE || 5,
+  },
+
+  resource: {
+    DEFAULT_IMAGE_PATH: ENV.DEFAULT_IMAGE_PATH ||  "/container/default/profile/profile-pic-icon-1.png"
+  },
+
+  database: {
+    DB_CONNECT: ENV.DB_CONNECT,
+  },
+
+  optimization: {
+    RATE_LIMIT_WINDOWMS: ENV.RATE_LIMIT_WINDOWMS || 15, //In minutes
+    RATE_LIMIT_MAX: ENV.RATE_LIMIT_MAX || 100,
+    COMPRESSION_LEVEL: ENV.COMPRESSION_LEVEL || 6,
+    COMPRESSION_MEMLEVEL: ENV.COMPRESSION_MEMLEVEL || 8,
+    COMPRESSION_CHUNKSIZE: ENV.COMPRESSION_CHUNKSIZE || 16384,
+    COMPRESSION_WINDOWBITS: ENV.COMPRESSION_WINDOWBITS || 15,
+    COMPRESSION_THRESHOLD_LIMIT: ENV.COMPRESSION_THRESHOLD_LIMIT || 0, //in bytes
+  },
+
 };
 
-const config = {
-  test: { ...test, ...defaults },
-  development: { ...development, ...defaults },
-  production: { ...production, ...defaults },
-};
+const config = { ...variables };
 
-export default config[env];
+export default config;
