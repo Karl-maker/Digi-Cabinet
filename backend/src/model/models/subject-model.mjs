6be13bcf262f509abd6d0b25ad1250754c33e826
karl-maker <karljohanbailey98@gmail.com>
2021-10-17 21:49:27 -0400

    Subject
    Model created for subject

---
+++ backend/src/model/models/subject-model.mjs
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+//--------------------------------------------------------------------------------
+
+const SubjectSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "Enter Name Of Subject"] trim: true },
+  description:{ type: String, required: [true, "Enter Name Of Subject"] trim: true },
+});
+
+const Subject = mongoose.model("Subjects", SubjectSchema);
+export default Subject;
