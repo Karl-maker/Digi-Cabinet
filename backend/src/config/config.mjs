9142e934e113facfff5e41e1c7ded99ec02189b7
karl-maker <karljohanbailey98@gmail.com>
2021-10-18 21:40:33 -0400

    ENV from process.env

--- backend/src/config/config.mjs
+++ backend/src/config/config.mjs
@@ -69,22 +69,22 @@ const variables = {
   },
 
   jwt: {
-    ISSUER: process.env.JWT_ISSUER || "Software Engineers United",
-    ALGORITHM: process.env.JWT_ALGORITHM || "RS256",
-    IS_HTTPS: process.env.JWT_IS_HTTPS || false, //This is usually false
-    REFRESH_TOKEN_LIFE: process.env.REFRESH_TOKEN_LIFE || 90,
-    ACCESS_TOKEN_LIFE: process.env.ACCESS_TOKEN_LIFE || 10000,
+    ISSUER: ENV.JWT_ISSUER || "Software Engineers United",
+    ALGORITHM: ENV.JWT_ALGORITHM || "RS256",
+    IS_HTTPS: ENV.JWT_IS_HTTPS || false, //This is usually false
+    REFRESH_TOKEN_LIFE: ENV.REFRESH_TOKEN_LIFE || 90,
+    ACCESS_TOKEN_LIFE: ENV.ACCESS_TOKEN_LIFE || 10000,
     ACCESS_TOKEN_PUBLIC_KEY: readENVFile(
-      process.env.ACCESS_TOKEN_PUBLIC_KEY || `../../keys/access-public.key`
+      ENV.ACCESS_TOKEN_PUBLIC_KEY || `../../keys/access-public.key`
     ),
     ACCESS_TOKEN_PRIVATE_KEY: readENVFile(
-      process.env.ACCESS_TOKEN_PRIVATE_KEY || "../../keys/access-private.key"
+      ENV.ACCESS_TOKEN_PRIVATE_KEY || "../../keys/access-private.key"
     ),
     REFRESH_TOKEN_PUBLIC_KEY: readENVFile(
-      process.env.REFRESH_TOKEN_PUBLIC_KEY || "../../keys/refresh-public.key"
+      ENV.REFRESH_TOKEN_PUBLIC_KEY || "../../keys/refresh-public.key"
     ),
     REFRESH_TOKEN_PRIVATE_KEY: readENVFile(
-      process.env.REFRESH_TOKEN_PRIVATE_KEY || "../../keys/refresh-private.key"
+      ENV.REFRESH_TOKEN_PRIVATE_KEY || "../../keys/refresh-private.key"
     ),
   },
 };
